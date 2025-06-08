fe9c38890da8344aa1b592d8e928c9ccb40213ab
Kacper Szafrański <kacperr.szafranski@gmail.com>
2025-06-08 16:43:28 +0200

    Initial commit

---
+++ src/test/java/org/mas/zoomanagementsystem/ZooManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mas.zoomanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
